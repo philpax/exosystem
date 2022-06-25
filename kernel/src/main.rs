196ca052db820a5ea0fa1020ff6833cf01208a9a
Philpax <me@philpax.me>
2022-06-25 18:40:12 +0200

    feat: wip wasm support

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -8,24 +8,28 @@ extern crate alloc;
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use exosystem::println;
+use exosystem::{allocator, memory};
 
 entry_point!(kernel_main);
 
+const HELLO_WORLD: &[u8] = include_bytes!("hello_world.wasm");
+
 #[allow(clippy::print_literal)]
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use exosystem::allocator;
-    use exosystem::memory::{self, BootInfoFrameAllocator};
-    use x86_64::VirtAddr;
-
-    println!("Hello World{}", "!");
     exosystem::init();
 
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let phys_mem_offset = x86_64::VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+    let mut frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let module = wasmi::Module::from_buffer(HELLO_WORLD).expect("failed to load wasm");
+    let _instance = wasmi::ModuleInstance::new(&module, &wasmi::ImportsBuilder::default())
+        .expect("failed to instantiate wasm module")
+        .run_start(&mut wasmi::NopExternals)
+        .expect("failed to execute start");
 
     #[cfg(test)]
     test_main();
@@ -37,6 +41,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    use exosystem::println;
     println!("{}", info);
     exosystem::hlt_loop();
 }
